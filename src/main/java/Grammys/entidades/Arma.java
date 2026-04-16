db759a01693850eceed4335677c4dc4690c430ac
catalinacrvln98-beep <catalinacrvln98@gmail.com>
2026-04-16 12:14:12 -0300

    Relaciones

--- src/main/java/Grammys/entidades/Arma.java
+++ src/main/java/Grammys/entidades/Arma.java
@@ -1,5 +1,41 @@
 package Grammys.entidades;
-
+import jakarta.persistence.*; 
+@Entity
 public class Arma {
+@Id 
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private int bonificadorDanio;
+    private float peso;
+
+    public Arma() {
+	}
+    public Long getId() {
+		return id;
+	}
+    public void setId(Long id) {
+		this.id = id;
+	}
+    public String getNombre() {
+		return nombre;
+	}
+	public void setNombre (String nombre) {
+        this.nombre = nombre;
+    }
+    public int getBonificadorDanio() {
+        return bonificadorDanio;
+    }
+    public void setBonificadorDanio(int bonificadorDanio) {
+        this.bonificadorDanio = bonificadorDanio;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    
+
 
 }
